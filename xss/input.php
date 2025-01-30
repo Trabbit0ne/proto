5411cb0c5add223111892f82039661dcb2a13d75
Trabbit0ne <trabbit0neyt@gmail.com>
2025-01-30 17:54:02 -0500

    Add files via upload

---
+++ xss/input.php
@@ -0,0 +1,11 @@
+<html>
+    <body>
+        <h1>Welcome to the website!</h1>
+        <p>Enter your name:</p>
+        <form>
+            <input type="text" name="name" />
+            <input type="submit" value="Submit" />
+        </form>
+        <p>Hello, <script>document.write(unescape(location.search.substring(1)))</script>!</p>
+    </body>
+</html>
